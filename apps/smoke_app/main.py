5da29976647c40add2a73a15fd4a10933893d4e2
arjunmehtaspec <117633064+arjunmehtaspec@users.noreply.github.com>
2026-04-15 16:50:00 +0530

    fix docker container run error

--- apps/smoke_app/main.py
+++ apps/smoke_app/main.py
@@ -1,7 +1,9 @@
-# apps/smoke_app/main.py
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Smoke Test OK 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
